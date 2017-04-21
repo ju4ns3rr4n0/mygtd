00f7507aa4e9ebab0ea3b1ea3eee882d38877f36
ju4ns3rr4n0 <ju4ns3rr4n0@gmail.com>
2017-04-21 23:09:21 +0200

    Modificado teplantillas para mostrar las tareas desde la BBDD.

--- gtd/views.py
+++ gtd/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Task
 
 # Create your views here.
 
 def task_list(request):
-    return render(request, 'gtd/task_list.html', {})
+	tasks = Task.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+	return render(request, 'gtd/task_list.html', {'tasks': tasks})
